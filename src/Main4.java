cd6a0fd62967f927e091926d37d963b81c791c09
Rustam <kutrustam1991@gmail.com>
2025-06-08 20:52:10 +0500

    commit3

---
+++ src/Main4.java
@@ -0,0 +1,9 @@
+public class Main4 {
+    public static void main(String[] args) {
+        int ticketPrice = 13_676;
+
+        int bonus = ticketPrice / 20;
+        System.out.println("Бонус: " + bonus);
+    }
+
+}
